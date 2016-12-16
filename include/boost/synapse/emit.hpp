38e503d2a6cbb371b82d8400f6280a2b56a26bf6
Emil Dotchevski <emil@revergestudios.com>
2016-12-15 18:25:20 -0800

    changed specialization of args_binder.

--- include/boost/synapse/emit.hpp
+++ include/boost/synapse/emit.hpp
@@ -59,10 +59,10 @@ boost
                 virtual void call( void const * ) const=0;
                 virtual int call_translated( void const * ) const=0;
                 };
-            template <class Signal,class=void,class=void,class=void,class=void> class args_binder;
+            template <class Signal,class CallSignature> class args_binder;
             template <class SigR,class SigA1,class A1>
             class
-            args_binder<SigR(*)(SigA1),A1>:
+            args_binder<SigR(*)(SigA1),void(*)(A1)>:
                 public args_binder_base
                 {
                 A1 a1;
@@ -74,7 +74,7 @@ boost
                 };
             template <class SigR,class SigA1,class SigA2,class A1,class A2>
             class
-            args_binder<SigR(*)(SigA1,SigA2),A1,A2>:
+            args_binder<SigR(*)(SigA1,SigA2),void(*)(A1,A2)>:
                 public args_binder_base
                 {
                 A1 a1; A2 a2;
@@ -86,7 +86,7 @@ boost
                 };
             template <class SigR,class SigA1,class SigA2,class SigA3,class A1,class A2,class A3>
             class
-            args_binder<SigR(*)(SigA1,SigA2,SigA3),A1,A2,A3>:
+            args_binder<SigR(*)(SigA1,SigA2,SigA3),void(*)(A1,A2,A3)>:
                 public args_binder_base
                 {
                 A1 a1; A2 a2; A3 a3;
@@ -98,7 +98,7 @@ boost
                 };
             template <class SigR,class SigA1,class SigA2,class SigA3,class SigA4,class A1,class A2,class A3,class A4>
             class
-            args_binder<SigR(*)(SigA1,SigA2,SigA3,SigA4),A1,A2,A3,A4>:
+            args_binder<SigR(*)(SigA1,SigA2,SigA3,SigA4),void(*)(A1,A2,A3,A4)>:
                 public args_binder_base
                 {
                 A1 a1; A2 a2; A3 a3; A4 a4;
@@ -150,25 +150,25 @@ boost
         typename synapse_detail::enable_if<signal_traits<Signal>::arity==1,int>::type
         emit( void const * e, A1 a1 )
             {
-            return synapse_detail::emit_fwd<Signal>(e,synapse_detail::args_binder<typename signal_traits<Signal>::signal_type,A1>(a1));
+            return synapse_detail::emit_fwd<Signal>(e,synapse_detail::args_binder<typename signal_traits<Signal>::signal_type,void(*)(A1)>(a1));
             }
         template <class Signal,class A1,class A2>
         typename synapse_detail::enable_if<signal_traits<Signal>::arity==2,int>::type
         emit( void const * e, A1 a1, A2 a2 )
             {
-            return synapse_detail::emit_fwd<Signal>(e,synapse_detail::args_binder<typename signal_traits<Signal>::signal_type,A1,A2>(a1,a2));
+            return synapse_detail::emit_fwd<Signal>(e,synapse_detail::args_binder<typename signal_traits<Signal>::signal_type,void(*)(A1,A2)>(a1,a2));
             }
         template <class Signal,class A1,class A2,class A3>
         typename synapse_detail::enable_if<signal_traits<Signal>::arity==3,int>::type
         emit( void const * e, A1 a1, A2 a2, A3 a3 )
             {
-            return synapse_detail::emit_fwd<Signal>(e,synapse_detail::args_binder<typename signal_traits<Signal>::signal_type,A1,A2,A3>(a1,a2,a3));
+            return synapse_detail::emit_fwd<Signal>(e,synapse_detail::args_binder<typename signal_traits<Signal>::signal_type,void(*)(A1,A2,A3)>(a1,a2,a3));
             }
         template <class Signal,class A1,class A2,class A3,class A4>
         typename synapse_detail::enable_if<signal_traits<Signal>::arity==4,int>::type
         emit( void const * e, A1 a1, A2 a2, A3 a3, A4 a4 )
             {
-            return synapse_detail::emit_fwd<Signal>(e,synapse_detail::args_binder<typename signal_traits<Signal>::signal_type,A1,A2,A3,A4>(a1,a2,a3,a4));
+            return synapse_detail::emit_fwd<Signal>(e,synapse_detail::args_binder<typename signal_traits<Signal>::signal_type,void(*)(A1,A2,A3,A4)>(a1,a2,a3,a4));
             }
         } 
     }
