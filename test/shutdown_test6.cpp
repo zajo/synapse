81e10ae05385361abcf6f05e2908e3a0d0352ef4
Emil Dotchevski <emildotchevski@gmail.com>
2018-01-02 13:50:35 -0800

    more shutdown tests

---
+++ test/shutdown_test6.cpp
@@ -0,0 +1,26 @@
+//Copyright (c) 2015-2017 Emil Dotchevski and Reverge Studios, Inc.
+
+//Distributed under the Boost Software License, Version 1.0. (See accompanying
+//file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <boost/synapse/connect.hpp>
+#include <boost/detail/lightweight_test.hpp>
+
+namespace synapse=boost::synapse;
+using synapse::shared_ptr;
+using synapse::make_shared;
+
+namespace
+    {
+    struct my_emitter_type { };
+    typedef struct my_signal_(*my_signal)();
+     shared_ptr<my_emitter_type> e=make_shared<my_emitter_type>();
+   }
+
+int
+main( int argc, char const * argv[] )
+    {
+    synapse::connect<synapse::meta::connected<my_signal> >( synapse::meta::emitter(), [ ]( synapse::connection &, unsigned ) { } );
+    synapse::connect<my_signal>( e, [ ] ( ) { } );
+    return boost::report_errors();
+    }
